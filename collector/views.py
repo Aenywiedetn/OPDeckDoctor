4820ffa340bbef9ca5a2ac6a11d6a857bee1863b
Rambowina <ramboakatemp@gmail.com>
2023-10-10 00:26:11 +0200

    Update

--- collector/views.py
+++ collector/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-  return render(request, 'collector/index.html')
+  return render(request, 'collector/filters.html')

00311d346b289fcc86377eaa07f50e52303f5683
Rambowina <ramboakatemp@gmail.com>
2023-11-03 23:48:30 +0100

    isotope out

--- OPDeckDoctor2/settings.py
+++ OPDeckDoctor2/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1z-jojf9xqwzu8#z%6ig@di(jm#hoqq1gnbpy_x02#b7-ygc(r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['opdeckdoctor.com', 'opdeckdoctor-production.up.railway.app', 'localhost']
+ALLOWED_HOSTS = ['opdeckdoctor.com', 'opdeckdoctor-production.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition

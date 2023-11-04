3e42612b24f41bdce3444b8e6e366964445eeffc
Rambowina <ramboakatemp@gmail.com>
2023-11-04 13:07:24 +0100

    allow host

--- OPDeckDoctor2/settings.py
+++ OPDeckDoctor2/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1z-jojf9xqwzu8#z%6ig@di(jm#hoqq1gnbpy_x02#b7-ygc(r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = ['opdeckdoctor.com', 'opdeckdoctor-production.up.railway.app', 'opdeckdoctor.up.railway.app' 'localhost', '127.0.0.1']
+DEBUG = False
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

abdc965d44e76c5d09ddc1b49b94a7a6a4113073
Rambowina <ramboakatemp@gmail.com>
2023-11-14 23:17:14 +0100

    Images to base64

---
+++ collector/tests.py
@@ -0,0 +1,24 @@
+from .models import Card
+import base64
+import os
+
+
+def convert_image_to_base64(image_path):
+    with open(image_path, 'rb') as img_file:
+        return base64.b64encode(img_file.read()).decode('utf-8')
+
+def fill_images_base64():
+    cards = Card.objects.all()  # Retrieve all cards from the database
+    img_folder = 'OPDeckDoctor2/static/img/'
+
+    for card in cards:
+        image_path = os.path.join(img_folder, f"{card.id}.png")
+
+        if os.path.exists(image_path):
+            base64_image = convert_image_to_base64(image_path)
+            card.image = base64_image
+            card.save()
+            print(f"Base64 image saved for Card {card.id}")
+        else:
+            print(f"Image not found for Card {card.id}")
+
